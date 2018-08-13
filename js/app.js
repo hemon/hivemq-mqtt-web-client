29876336951f6add18b7ae1242f45b27780b9700
Zhangwei <hemono@gmail.com>
2018-08-13 16:01:03 +0800

    * 去掉js外部依赖

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
-/**
- * Copyright 2013 dc-square GmbH
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author: Christoph Schäbel
- */
-
 var websocketclient = {
     'client': null,
     'lastMessageId': 1,
@@ -185,7 +167,7 @@ var websocketclient = {
     'deleteSubscription': function (id) {
         var elem = $("#sub" + id);
 
-        if (confirm('Are you sure ?')) {
+        if (confirm('Wirklich löschen ?')) {
             elem.remove();
             this.unsubscribe(id);
         }
